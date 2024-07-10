e7f9bd552ea3bf7b842854396f733e6d74bc4f71
shaikKarimulla59 <karimulla1420626@gmail.com>
2024-07-10 17:21:12 +0530

    only src

---
+++ test/letstest.java
@@ -0,0 +1,16 @@
+package test;
+
+public class letstest {
+
+	public static void main(String[] args) {
+		System.out.println("lets test it");
+		System.out.println("lets test it");
+		System.out.println("lets test it");
+		System.out.println("lets test it");
+		System.out.println("lets test it");
+		System.out.println("lets test it");
+		System.out.println("lets test it");
+		
+	}
+
+}
